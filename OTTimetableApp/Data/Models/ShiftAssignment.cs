57f247b2f268d4f5bb9235d4755cbb74b115e777
Syed Azlan <syedazlan5169@gmail.com>
2026-03-02 15:58:16 +0800

    added ShiftAssignment class

---
+++ OTTimetableApp/Data/Models/ShiftAssignment.cs
@@ -0,0 +1,15 @@
+﻿namespace OTTimetableApp.Data.Models;
+
+public class ShiftAssignment
+{
+    public int Id { get; set; }
+
+    public int CalendarDayId { get; set; }
+    public CalendarDay CalendarDay { get; set; } = null!;
+
+    public ShiftType ShiftType { get; set; }
+
+    // Locked: which group owns this shift (from CalendarDay)
+    public int GroupId { get; set; }
+    public Group Group { get; set; } = null!;
+}
