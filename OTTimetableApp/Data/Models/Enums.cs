e568822e5f0a3af34ad176bf91dd6841d7a20ec1
Syed Azlan <syedazlan5169@gmail.com>
2026-03-02 15:56:25 +0800

    added Enums class

---
+++ OTTimetableApp/Data/Models/Enums.cs
@@ -0,0 +1,16 @@
+﻿namespace OTTimetableApp.Data.Models;
+
+public enum ShiftType
+{
+    Night = 1,   // 22:00-07:00
+    Morning = 2, // 07:00-15:00
+    Evening = 3  // 14:00-23:00
+}
+
+public enum SlotFillType
+{
+    Planned = 1,      // default from base group member
+    Replacement = 2,  // replaced someone on leave
+    EmptyFill = 3,    // filled an originally empty warrant
+    Empty = 4         // no one assigned
+}
