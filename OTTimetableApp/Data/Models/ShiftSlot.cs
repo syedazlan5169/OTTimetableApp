7fc8fb0816f99e72626baa40e7c064eb94a40a4f
Syed Azlan <syedazlan5169@gmail.com>
2026-03-02 16:00:08 +0800

    Added ShiftSlot class

---
+++ OTTimetableApp/Data/Models/ShiftSlot.cs
@@ -0,0 +1,26 @@
+﻿namespace OTTimetableApp.Data.Models;
+
+public class ShiftSlot
+{
+    public int Id { get; set; }
+
+    public int ShiftAssignmentId { get; set; }
+    public ShiftAssignment ShiftAssignment { get; set; } = null!;
+
+    // 1..5 (warrant slot)
+    public int SlotIndex { get; set; }
+
+    // Planned employee (from base group), can be null if empty warrant
+    public int? PlannedEmployeeId { get; set; }
+    public Employee? PlannedEmployee { get; set; }
+
+    // Actual employee assigned (user edits this)
+    public int? ActualEmployeeId { get; set; }
+    public Employee? ActualEmployee { get; set; }
+
+    // Audit: if replacement, who got replaced
+    public int? ReplacedEmployeeId { get; set; }
+    public Employee? ReplacedEmployee { get; set; }
+
+    public SlotFillType FillType { get; set; } = SlotFillType.Planned;
+}
