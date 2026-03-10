36d6547b6ffbaa8c882fd42da1e8eee09f1612ea
Syed Azlan <syedazlan5169@gmail.com>
2026-03-10 12:49:03 +0800

    All checked/uncheck feature. Live update when checked/uncheck ot line

--- OTTimetableApp/ViewModels/ClaimPreviewVM.cs
+++ OTTimetableApp/ViewModels/ClaimPreviewVM.cs
@@ -10,17 +10,41 @@ public partial class ClaimPreviewVM : ObservableObject
     private readonly MonthViewService _monthSvc;
     private readonly EmployeeService _empSvc;
     private readonly OtCalculatorService _otSvc;
-    public decimal Total1125 => Lines.Sum(x => x.H1125 ?? 0);
-    public decimal Total125 => Lines.Sum(x => x.H125 ?? 0);
-    public decimal Total15 => Lines.Sum(x => x.H15 ?? 0);
-    public decimal Total175 => Lines.Sum(x => x.H175 ?? 0);
-    public decimal Total20 => Lines.Sum(x => x.H20 ?? 0);
+    public decimal Total1125 => Lines.Where(x => x.IsChecked).Sum(x => x.H1125 ?? 0);
+    public decimal Total125 => Lines.Where(x => x.IsChecked).Sum(x => x.H125 ?? 0);
+    public decimal Total15 => Lines.Where(x => x.IsChecked).Sum(x => x.H15 ?? 0);
+    public decimal Total175 => Lines.Where(x => x.IsChecked).Sum(x => x.H175 ?? 0);
+    public decimal Total20 => Lines.Where(x => x.IsChecked).Sum(x => x.H20 ?? 0);
     public decimal Claim1125 => Total1125 * 1.125m * HourlyRate;
     public decimal Claim125 => Total125 * 1.25m * HourlyRate;
     public decimal Claim15 => Total15 * 1.5m * HourlyRate;
     public decimal Claim175 => Total175 * 1.75m * HourlyRate;
     public decimal Claim20 => Total20 * 2.0m * HourlyRate;
     public decimal GrandTotal => Claim1125 + Claim125 + Claim15 + Claim175 + Claim20;
+
+    private bool _isBulkUpdating;
+
+    private bool _allChecked = true;
+    public bool AllChecked
+    {
+        get => _allChecked;
+        set
+        {
+            if (_allChecked == value) return;
+
+            _allChecked = value;
+            OnPropertyChanged();
+
+            if (_isBulkUpdating) return;
+
+            _isBulkUpdating = true;
+            foreach (var line in Lines)
+                line.IsChecked = value;
+            _isBulkUpdating = false;
+
+            RefreshTotals();
+        }
+    }
 
     public ObservableCollection<CalendarOptionVM> Calendars { get; } = new();
     public ObservableCollection<MonthOptionVM> Months { get; } =
@@ -42,6 +66,46 @@ public partial class ClaimPreviewVM : ObservableObject
         _monthSvc = monthSvc;
         _empSvc = empSvc;
         _otSvc = otSvc;
+    }
+
+    private void RefreshTotals()
+    {
+        OnPropertyChanged(nameof(Total1125));
+        OnPropertyChanged(nameof(Total125));
+        OnPropertyChanged(nameof(Total15));
+        OnPropertyChanged(nameof(Total175));
+        OnPropertyChanged(nameof(Total20));
+
+        OnPropertyChanged(nameof(Claim1125));
+        OnPropertyChanged(nameof(Claim125));
+        OnPropertyChanged(nameof(Claim15));
+        OnPropertyChanged(nameof(Claim175));
+        OnPropertyChanged(nameof(Claim20));
+
+        OnPropertyChanged(nameof(GrandTotal));
+        OnPropertyChanged(nameof(HourlyRate));
+    }
+
+    private void AttachLineHandlers()
+    {
+        foreach (var line in Lines)
+        {
+            line.PropertyChanged -= Line_PropertyChanged;
+            line.PropertyChanged += Line_PropertyChanged;
+        }
+    }
+
+    private void Line_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName != nameof(ClaimLineVM.IsChecked)) return;
+
+        if (!_isBulkUpdating)
+        {
+            _allChecked = Lines.Count > 0 && Lines.All(x => x.IsChecked);
+            OnPropertyChanged(nameof(AllChecked));
+        }
+
+        RefreshTotals();
     }
 
     public decimal HourlyRate
@@ -133,7 +197,13 @@ public partial class ClaimPreviewVM : ObservableObject
 
         OnPropertyChanged(nameof(GrandTotal));
         OnPropertyChanged(nameof(HourlyRate));
+
+        AllChecked = true;
+        AttachLineHandlers();
+        RefreshTotals();
     }
+
+
 }
 
 public class EmployeePick
