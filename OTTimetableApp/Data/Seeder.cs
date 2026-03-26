37147a712cfa8f7140e65f9b9c7682058d5fd7bb
logicgame1001 <logicgame1001@gmail.com>
2026-03-27 06:55:19 +0800

    update initial name seeder

--- OTTimetableApp/Data/Seeder.cs
+++ OTTimetableApp/Data/Seeder.cs
@@ -17,10 +17,10 @@ public static class Seeder
         // 1) Employees
         var employeeNames = new[]
         {
-            "Shawal","Sakinah","Syed","Faiz",
-            "Zahalan","Basrul","Azlan","Zikry",
-            "Baiti","Hamizi","Asrul",
-            "Zaini","Faizuddin","Mirza","Hafiz"
+            "Employee1","Employee2","Employee3","Employee4",
+            "Employee5","Employee6","Employee7","Employee8",
+            "Employee9","Employee10","Employee11","Employee12",
+            "Employee13","Employee14","Employee15","Employee16"
         };
 
         var employees = employeeNames
@@ -44,37 +44,37 @@ public static class Seeder
         // 3) Group Members (SlotIndex 1..5)
         // KUMPULAN A: Shawal, Sakinah, Syed, Faiz, EMPTY
         db.GroupMembers.AddRange(
-            new GroupMember { GroupId = gA.Id, SlotIndex = 1, EmployeeId = EmpId("Shawal") },
-            new GroupMember { GroupId = gA.Id, SlotIndex = 2, EmployeeId = EmpId("Sakinah") },
-            new GroupMember { GroupId = gA.Id, SlotIndex = 3, EmployeeId = EmpId("Syed") },
-            new GroupMember { GroupId = gA.Id, SlotIndex = 4, EmployeeId = EmpId("Faiz") },
+            new GroupMember { GroupId = gA.Id, SlotIndex = 1, EmployeeId = EmpId("Employee1") },
+            new GroupMember { GroupId = gA.Id, SlotIndex = 2, EmployeeId = EmpId("Employee2") },
+            new GroupMember { GroupId = gA.Id, SlotIndex = 3, EmployeeId = EmpId("Employee3") },
+            new GroupMember { GroupId = gA.Id, SlotIndex = 4, EmployeeId = EmpId("Employee4") },
             new GroupMember { GroupId = gA.Id, SlotIndex = 5, EmployeeId = null }
         );
 
         // KUMPULAN B: Zahalan, Basrul, Azlan, Zikry, EMPTY
         db.GroupMembers.AddRange(
-            new GroupMember { GroupId = gB.Id, SlotIndex = 1, EmployeeId = EmpId("Zahalan") },
-            new GroupMember { GroupId = gB.Id, SlotIndex = 2, EmployeeId = EmpId("Basrul") },
-            new GroupMember { GroupId = gB.Id, SlotIndex = 3, EmployeeId = EmpId("Azlan") },
-            new GroupMember { GroupId = gB.Id, SlotIndex = 4, EmployeeId = EmpId("Zikry") },
+            new GroupMember { GroupId = gB.Id, SlotIndex = 1, EmployeeId = EmpId("Employee5") },
+            new GroupMember { GroupId = gB.Id, SlotIndex = 2, EmployeeId = EmpId("Employee6") },
+            new GroupMember { GroupId = gB.Id, SlotIndex = 3, EmployeeId = EmpId("Employee7") },
+            new GroupMember { GroupId = gB.Id, SlotIndex = 4, EmployeeId = EmpId("Employee8") },
             new GroupMember { GroupId = gB.Id, SlotIndex = 5, EmployeeId = null }
         );
 
         // KUMPULAN C: Baiti, Hamizi, Asrul, EMPTY, EMPTY
         db.GroupMembers.AddRange(
-            new GroupMember { GroupId = gC.Id, SlotIndex = 1, EmployeeId = EmpId("Baiti") },
-            new GroupMember { GroupId = gC.Id, SlotIndex = 2, EmployeeId = EmpId("Hamizi") },
-            new GroupMember { GroupId = gC.Id, SlotIndex = 3, EmployeeId = EmpId("Asrul") },
-            new GroupMember { GroupId = gC.Id, SlotIndex = 4, EmployeeId = null },
+            new GroupMember { GroupId = gC.Id, SlotIndex = 1, EmployeeId = EmpId("Employee9") },
+            new GroupMember { GroupId = gC.Id, SlotIndex = 2, EmployeeId = EmpId("Employee10") },
+            new GroupMember { GroupId = gC.Id, SlotIndex = 3, EmployeeId = EmpId("Employee11") },
+            new GroupMember { GroupId = gC.Id, SlotIndex = 4, EmployeeId = EmpId("Employee12") },
             new GroupMember { GroupId = gC.Id, SlotIndex = 5, EmployeeId = null }
         );
 
         // KUMPULAN D: Zaini, Faizuddin, Mirza, Hafiz, EMPTY
         db.GroupMembers.AddRange(
-            new GroupMember { GroupId = gD.Id, SlotIndex = 1, EmployeeId = EmpId("Zaini") },
-            new GroupMember { GroupId = gD.Id, SlotIndex = 2, EmployeeId = EmpId("Faizuddin") },
-            new GroupMember { GroupId = gD.Id, SlotIndex = 3, EmployeeId = EmpId("Mirza") },
-            new GroupMember { GroupId = gD.Id, SlotIndex = 4, EmployeeId = EmpId("Hafiz") },
+            new GroupMember { GroupId = gD.Id, SlotIndex = 1, EmployeeId = EmpId("Employee13") },
+            new GroupMember { GroupId = gD.Id, SlotIndex = 2, EmployeeId = EmpId("Employee14") },
+            new GroupMember { GroupId = gD.Id, SlotIndex = 3, EmployeeId = EmpId("Employee15") },
+            new GroupMember { GroupId = gD.Id, SlotIndex = 4, EmployeeId = EmpId("Employee16") },
             new GroupMember { GroupId = gD.Id, SlotIndex = 5, EmployeeId = null }
         );
 
